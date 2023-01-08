bcbf7598e74a7b4be6db7b617102466f12cf38b7
Bob Coret <bob.coret+github@gmail.com>
2023-01-08 20:39:12 +0100

    informatie in de tijd variabel gemaakt
    bestand timeBoundInformation voorzien van datasets uit het collectiebeheersysteem die via SPARQL worden opgehaald al naar gelang de keuze van de gebruiker

--- data/timeBoundInformation.js
+++ data/timeBoundInformation.js
@@ -1,19 +1,44 @@
-const timeBoundInformation = [
-	{
+const timeBoundInformation = {
+	"baljuwen": {
+		"title": "Baljuwen",
+		"yearFrom": 1334,
+		"yearUntil": 1571,
+		"itemset": "https://n2t.net/ark:/60537/bfCzjO"
+	}, 
+	"schouten": {
+		"title": "Schouten",
+		"yearFrom": 1359,
+		"yearUntil": 1572,
+		"itemset":  "https://n2t.net/ark:/60537/bELKeb"
+	}, 
+	"kasteleins": {
+		"title": "Kasteleins",
+		"yearFrom": 1391,
+		"yearUntil": 1571,
+		"itemset":  "https://n2t.net/ark:/60537/by6mb1"
+	}, 
+	"kerkmeesters": {
+		"title": "Kerkmeesters",
+		"yearFrom": 1423,
+		"yearUntil": 1589,
+		"itemset": "https://n2t.net/ark:/60537/bixn3C"
+	}, 
+	"gasthuismeesters": {
+		"title": "Gasthuismeesters",
+		"yearFrom": 1484,
+		"yearUntil": 1589,
+		"itemset":  "https://n2t.net/ark:/60537/bNVxwM"
+	}, 
+	"heiligegeestmeesters": {
+		"title": "Heilige Geestmeesters",
+		"yearFrom": 1484,
+		"yearUntil": 1589,
+		"itemset": "https://n2t.net/ark:/60537/b9nzKv"
+	}, 
+	"burgemeesters": {
 		"title": "Burgemeesters",
 		"yearFrom": 1502,
-		"yearUntil": 2022
-	}, {
-		"title": "Kerkmeesters",
-		"yearFrom": 1484,
-		"yearUntil": 1589
-	}, {
-		"title": "Schepenen",
-		"yearFrom": 1314,
-		"yearUntil": 1573
-	}, {
-		"title": "Tresoriers",
-		"yearFrom": 1436,
-		"yearUntil": 1589
+		"yearUntil": 2022,
+		"itemset":"https://n2t.net/ark:/60537/bPoosY"
 	}
-];
+};
